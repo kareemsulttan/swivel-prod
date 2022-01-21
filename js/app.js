8ba2e1e3a0ad75c16d238a58c1ec19abe8a97e91
Kareem Sultan <76824154+kareemsulttan@users.noreply.github.com>
2022-01-21 14:30:41 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,3 +2,20 @@
 $(".selectbox-menu .dropdown-links-list > li > a").click(function () {
   $(".selectbox-menu > .btn").text($(this).text());
 });
+
+$('.pagination li a.current-link').on('click', function(){
+  $('.pagination li a.current-link.active').removeClass('active');
+  $(this).addClass('active');
+});
+
+
+$('.btn-plus, .btn-minus').on('click', function (e) {
+  const isNegative = $(e.target).closest('.btn-minus').is('.btn-minus');
+  const input = $(e.target).closest('.form-group').find('input');
+  if (input.is('input')) {
+    input[0][isNegative ? 'stepDown' : 'stepUp']()
+  }
+})
+
+
+
